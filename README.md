a6eb667f9a02cb43c52efd4584355cef7220b621
Rafa Barberá <rbarberac@gmail.com>
2013-01-09 17:27:12 +0100

    verbs-freq.plist generated with the frequency of use added

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Compute frequency of use of irregular engish verbs. Data set for ["A list of verbs"](https://github.com/rbarbera/irregularverbs) iOS App.
 
 The fequency word data comes from <http://invokeit.wordpress.com/frequency-word-lists/>
+
+## Dependencies
+
+[unicodecsv](https://github.com/jdunck/python-unicodecsv) for writing CSV files with UTF-8 encoded strings
--- freq-verbs/ComputeVerbFrequency.py
+++ freq-verbs/ComputeVerbFrequency.py
@@ -1,27 +1,63 @@
 import fileinput
 import plistlib
+import unicodecsv
 
+
+# Utility functions used to convert from/to a CSV to edit. 
+# For example
+# 	call verbsPlistToCsv('verbs-org.plist','verbs.csv')
+#	edit 'verbs.csv' with Excel, Numbers, etc
+#	call csvToVerbsPlist('verbs.csv','verbs-edited.plist')
+
+def	verbsPlistToCsv(plistPath, csvPath):
+	plist = plistlib.readPlist(plistPath)
+	outFile = open(csvPath,'w')
+	csvWriter = unicodecsv.writer(outFile,encoding='utf-8',delimiter=';')
+	for item in plist:
+		values = item.values()
+		csvWriter.writerow(values)
+	outFile.close()
+
+def csvToVerbsPlist(csvPath, plistPath):
+    inFile = open(csvPath,'r');
+    csvReader = unicodecsv.reader(inFile, encoding='utf-8', delimiter=';')
+    plist = []
+    for row in csvReader:
+        plist.append({'simple':row[0].lower(),'past':row[1].lower(),
+        'translation':row[2].lower(),'participle':row[3].lower(),'level':int(row[4])})
+    plistlib.writePlist(plist,plistPath)
+		
+
+# Read 50K word list with use count
 freq50K = {}
 for line in fileinput.input('en_50K.txt'):
-	#we need to remove the original EOL
+	# we need to remove the original EOL
 	line = line[:len(line)-2].split(' ')
 	freq50K.update({line[0]:int(line[1])})
 
 
-freqVerbs = {}	
-verbs = plistlib.readPlist('verbs.plist')
+# Read verbs.plist from "A list of verbs" and compute use count for all tenses
+# We assume that the verbs with multiple forms are in the form "do/does"
+verbs = plistlib.readPlist('verbs-edited.plist')
 for verb in verbs:
-	verbalForm = verb.values()
-	count = freq50K.get(verbalForm[0],0) 	# simple
-	count += freq50K.get(verbalForm[1],0) 	# past
-	count += freq50K.get(verbalForm[3],0) 	# participle
-	freqVerbs.update({verbalForm[0]:count})
+	# We join all the verbal forms to construct a string like this:
+	# was/were/be/am/are/is/been
+	testValues = '/'.join([verb['simple'],verb['past'],verb['participle']])
+	count = 0;
+	# Now we look for each word and add the use count
+	for tense in testValues.split('/'):
+		count += freq50K.get(tense,0);
+	verb.update({'useCount':count})
 	
-list = []
-for v,f in freqVerbs.items():
-	list.append("%010d%s"%(f,v));
-	
-list.sort()
-	
-print list
+# Compute frequencies
+total = reduce(lambda x,y: x+y['useCount'] ,verbs,0)
+for verb in verbs:
+	verb.update({'frequency': 1.0*verb['useCount']/total})
+	del verb['useCount'];
+
+# Save a new verbs.plist with the frequency of use
+plistlib.writePlist(verbs,'verbs-freq.plist')
+
+# Save also a csv copy to statistical analysis
+verbsPlistToCsv('verbs-freq.plist','verbs-freq.csv')
 
